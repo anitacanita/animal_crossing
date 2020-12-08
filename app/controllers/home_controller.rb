d237fc4dc77cdd0073d9549bc2e3df633f7cad3e
Ana Carlos <adscarlos@gmail.com>
2020-12-08 20:20:23 +0000

    Generate partial dynamically, move data into yaml file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,24 @@
+require 'yaml'
 class HomeController < ApplicationController
+
+
   def index
-    @villagers = [['Hazel', 'hazel'], ['Sterling', 'sterling'], ['Poppy', 'poppy'], ['Pango', 'pango']]
+    @villagers = villagers_data.map { | villager, data | [data['Name'], villager] }
   end
 
   def show_villager
     @villager_name = params['villager_name'].downcase
+    @species = villagers_data[@villager_name]['Species']
+    @personality = villagers_data[@villager_name]['Personality']
+    @gender = villagers_data[@villager_name]['Gender']
+    @birthday = villagers_data[@villager_name]['Birthday']
+    @quote = villagers_data[@villager_name]['Quote']
+    @catchphrase = villagers_data[@villager_name]['Catchphrase']
+  end
+
+  private
+
+  def villagers_data
+    @villagers_data ||= YAML.safe_load(File.read('config/data/villagers.yml'))
   end
 end
