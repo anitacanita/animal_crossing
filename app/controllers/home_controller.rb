1680f25c706ece615f2e78fc9e682cd05b525345
Ana Carlos <adscarlos@gmail.com>
2020-12-09 15:22:54 +0000

    Refactor home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,27 @@
-require 'yaml'
 class HomeController < ApplicationController
-
-
   def index
     @villagers = villagers_data.map { | villager, data | [data['Name'], villager] }
   end
 
   def show_villager
     @villager_name = params['villager_name'].downcase
-    @species = villagers_data[@villager_name]['Species']
-    @personality = villagers_data[@villager_name]['Personality']
-    @gender = villagers_data[@villager_name]['Gender']
-    @birthday = villagers_data[@villager_name]['Birthday']
-    @quote = villagers_data[@villager_name]['Quote']
-    @catchphrase = villagers_data[@villager_name]['Catchphrase']
+    get_villager_data_for(@villager_name)
   end
 
   private
 
   def villagers_data
     @villagers_data ||= YAML.safe_load(File.read('config/data/villagers.yml'))
+  end
+
+  def get_villager_data_for(name)
+    villager = villagers_data[name]
+
+    @species = villager['Species']
+    @personality = villager['Personality']
+    @gender = villager['Gender']
+    @birthday = villager['Birthday']
+    @quote = villager['Quote']
+    @catchphrase = villager['Catchphrase']
   end
 end
